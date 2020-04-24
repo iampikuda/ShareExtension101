87de46b5b558cb04b7cde332d5d7c114991647c1
Damisi Pikuda <damisipikuda@gmail.com>
2020-04-24 15:27:54 +0100

    Add share extension target

--- ShareExtension101/ViewController.swift
+++ ShareExtension101/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
