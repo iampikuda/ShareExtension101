e4a58a35d4fd38827372000012b9bd0f4abd850b
Damisi Pikuda <damisipikuda@gmail.com>
2020-04-29 15:44:24 +0100

    Update scenedel

--- ShareExtension101/SceneDelegate.swift
+++ ShareExtension101/SceneDelegate.swift
@@ -19,28 +19,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let _ = (scene as? UIWindowScene) else { return }
-        if let url = connectionOptions.urlContexts.first?.url,
-            let scheme = url.scheme,
-            scheme.caseInsensitiveCompare("ShareExtension101") == .orderedSame,
-            let page = url.host {
-
-            var parameters: [String: String] = [:]
-            URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach {
-                parameters[$0.name] = $0.value
-            }
-
-            print("redirect(to: \(page), with: \(parameters))")
-
-            for parameter in parameters where parameter.key.caseInsensitiveCompare("url") == .orderedSame {
-                UserDefaults().set(parameter.value, forKey: "incomingURL")
-            }
+        if let url = connectionOptions.urlContexts.first?.url {
+            handleIncomingURL(url)
         }
     }
 
     // Called on existing scenes
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        if let url = URLContexts.first?.url,
-            let scheme = url.scheme,
+        if let url = URLContexts.first?.url {
+            handleIncomingURL(url)
+        }
+    }
+
+    func handleIncomingURL(_ url: URL) {
+        if let scheme = url.scheme,
             scheme.caseInsensitiveCompare("ShareExtension101") == .orderedSame,
             let page = url.host {
 
--- ShareExtension101Share/ShareViewController.swift
+++ ShareExtension101Share/ShareViewController.swift
@@ -12,19 +12,11 @@ import CoreServices
 
 class ShareViewController: UIViewController {
 
-    // Courtesy: https://stackoverflow.com/a/44499222/13363449 👇🏾
-    // Function must be named exactly like this so a selector can be found by the compiler!
-    // Anyway - it's another selector in another instance that would be "performed" instead.
-    @objc func openURL(_ url: URL) -> Bool {
-        var responder: UIResponder? = self
-        while responder != nil {
-            if let application = responder as? UIApplication {
-                return application.perform(#selector(openURL(_:)), with: url) != nil
-            }
-            responder = responder?.next
-        }
-        return false
-    }
+    private let typeText = String(kUTTypeText)
+    private let typeURL = String(kUTTypeURL)
+    private let appURL = "ShareExtension101://"
+    private let groupName = "group.ShareExtension101"
+    private let urlDefaultName = "incomingURL"
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -34,54 +26,77 @@ class ShareViewController: UIViewController {
                 self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
                 return
         }
-        let typeText = String(kUTTypeText)
-        let typeURL = String(kUTTypeURL)
 
-        // 2
         if itemProvider.hasItemConformingToTypeIdentifier(typeText) {
-            itemProvider.loadItem(forTypeIdentifier: typeText, options: nil) { (item, error) in
-                if let error = error { print("Text-Error: \(error.localizedDescription)") }
-
-
-                if let text = item as? String {
-                    do {// 2.1
-                        let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-                        let matches = detector.matches(
-                            in: text,
-                            options: [],
-                            range: NSRange(location: 0, length: text.utf16.count)
-                        )
-                        // 2.2
-                        if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
-                            UserDefaults(suiteName: "group.ShareExtension101")?.set(text[range], forKey: "incomingURL")
-                        }
-                    } catch let error {
-                        print("Do-Try Error: \(error.localizedDescription)")
-                    }
-                }
-
-                self.extensionContext?.completeRequest(returningItems: nil, completionHandler: { _ in
-                    guard let url = URL(string: "ShareExtension101://") else { return }
-                    _ = self.openURL(url)
-                })
-            }
-        // 3
+            handleIncomingText(itemProvider: itemProvider)
         } else if itemProvider.hasItemConformingToTypeIdentifier(typeURL) {
-            itemProvider.loadItem(forTypeIdentifier: typeURL, options: nil) { (item, error) in
-                if let error = error { print("URL-Error: \(error.localizedDescription)") }
-
-                if let url = item as? NSURL, let urlString = url.absoluteString {
-                    UserDefaults(suiteName: "group.ShareExtension101")?.set(urlString, forKey: "incomingURL")
-                }
-
-                self.extensionContext?.completeRequest(returningItems: nil, completionHandler: { _ in
-                    guard let url = URL(string: "ShareExtension101://") else { return }
-                    _ = self.openURL(url)
-                })
-            }
+            handleIncomingURL(itemProvider: itemProvider)
         } else {
             print("Error: No url or text found")
             self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
         }
+    }
+
+    private func handleIncomingText(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeText, options: nil) { (item, error) in
+            if let error = error { print("Text-Error: \(error.localizedDescription)") }
+
+
+            if let text = item as? String {
+                do {// 2.1
+                    let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+                    let matches = detector.matches(
+                        in: text,
+                        options: [],
+                        range: NSRange(location: 0, length: text.utf16.count)
+                    )
+                    // 2.2
+                    if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
+                        self.saveURLString(String(text[range]))
+                    }
+                } catch let error {
+                    print("Do-Try Error: \(error.localizedDescription)")
+                }
+            }
+
+            self.openMainApp()
+        }
+    }
+
+    private func handleIncomingURL(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeURL, options: nil) { (item, error) in
+            if let error = error { print("URL-Error: \(error.localizedDescription)") }
+
+            if let url = item as? NSURL, let urlString = url.absoluteString {
+                self.saveURLString(urlString)
+            }
+
+            self.openMainApp()
+        }
+    }
+
+    private func saveURLString(_ urlString: String) {
+        UserDefaults(suiteName: self.groupName)?.set(urlString, forKey: self.urlDefaultName)
+    }
+
+    private func openMainApp() {
+        self.extensionContext?.completeRequest(returningItems: nil, completionHandler: { _ in
+            guard let url = URL(string: self.appURL) else { return }
+            _ = self.openURL(url)
+        })
+    }
+
+    // Courtesy: https://stackoverflow.com/a/44499222/13363449 👇🏾
+    // Function must be named exactly like this so a selector can be found by the compiler!
+    // Anyway - it's another selector in another instance that would be "performed" instead.
+    @objc private func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                return application.perform(#selector(openURL(_:)), with: url) != nil
+            }
+            responder = responder?.next
+        }
+        return false
     }
 }
